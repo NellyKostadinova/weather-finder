d44063edece429c00be3aec8ff410911b04db209
Neli Kostadinova <nkostadinova@hhogdev.com>
2019-12-03 09:49:22 +0200

    Updates

---
+++ precache-manifest.240e2717df856dba10024911d47e8f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df74369eb848917a6a6c5516a6de10c",
+    "url": "/weather-finder/index.html"
+  },
+  {
+    "revision": "2b6894d71f11159b21cb",
+    "url": "/weather-finder/static/css/main.b8ee0447.chunk.css"
+  },
+  {
+    "revision": "f347b79aa194caf95b65",
+    "url": "/weather-finder/static/js/2.1ad8279b.chunk.js"
+  },
+  {
+    "revision": "2b6894d71f11159b21cb",
+    "url": "/weather-finder/static/js/main.0753cc51.chunk.js"
+  },
+  {
+    "revision": "d45247e81f1bc5e745bd",
+    "url": "/weather-finder/static/js/runtime-main.bf96fe8e.js"
+  },
+  {
+    "revision": "0e8c8c30367b3e7f7b8b0b1fda37c07d",
+    "url": "/weather-finder/static/media/marker.0e8c8c30.svg"
+  }
+]);
